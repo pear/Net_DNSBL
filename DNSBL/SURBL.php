cac34ad3a44156aac66748cbe0c357558a4bb1e9
Sebastian Nohn <nohn@php.net>
2004-11-28 16:30:51 +0000

    I can't believe that prima donna like behaviour
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_DNSBL/trunk@173688 c90b9560-bf6c-de11-be94-00142212c4b1

--- DNSBL/SURBL.php
+++ DNSBL/SURBL.php
@@ -29,7 +29,7 @@
  * @author  Sebastian Nohn <sebastian@nohn.net>
  * @package Net_DNSBL
  * @license http://www.php.net/license/3_0.txt
- * @version 1.0.0
+ * @version 0.5.1
  */
 require_once 'Cache.php';
 require_once 'HTTP/Request.php';
