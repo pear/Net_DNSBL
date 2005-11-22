7c08735d31062b12c938161797b7dfb5490dfd43
Sebastian Nohn <nohn@php.net>
2005-11-22 12:27:59 +0000

    whereever that double header crap came from... fixing now
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_DNSBL/trunk@201038 c90b9560-bf6c-de11-be94-00142212c4b1

--- DNSBL.php
+++ DNSBL.php
@@ -3,33 +3,6 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * Short description for file
- *
- * Long description for file (if any)...
- *
- * PHP versions 4 and 5
- *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category   CategoryName
- * @package    PackageName
- * @author     Original Author <author@example.com>
- * @author     Another Author <another@example.com>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/PackageName
- * @see        NetOther, Net_Sample::Net_Sample()
- * @since      File available since Release 1.2.0
- * @deprecated File deprecated in Release 2.0.0
- */
-
-
-/**
  * PEAR::Net_DNSBL
  *
  * This class acts as interface to generic Realtime Blocking Lists
