e8c12aa75a618b4046d74f0212a35837848e2ba9
Sebastian Nohn <nohn@php.net>
2006-03-10 17:10:53 +0000

    * fix tests
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_DNSBL/trunk@209074 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/testNetDNSBL.php
+++ tests/testNetDNSBL.php
@@ -14,19 +14,20 @@ class testNetDNSBL extends PHPUnit2_Framework_TestCase {
     }
 
     public function testTrustworthyHostsArentListed() {
+        $this->rbl->setBlacklists(array('dun.dnsrbl.net'));
         $this->assertEquals(false, $this->rbl->isListed("mail.nohn.net"));
         $this->assertEquals(false, $this->rbl->isListed("212.112.226.205"));
         $this->assertEquals(false, $this->rbl->isListed("smtp1.google.com"));
     }
 
     public function testSetters() {
-        $this->rbl->setRBL(array('dun.dnsrbl.net'));
-        $this->assertEquals(array('dun.dnsrbl.net'), $this->rbl->getRBL());
+        $this->rbl->setBlacklists(array('dun.dnsrbl.net'));
+        $this->assertEquals(array('dun.dnsrbl.net'), $this->rbl->getBlacklists());
     }
 
     public function testSettersAndLookups() {
-        $this->rbl->setRBL(array('dnsbl.sorbs.net'));
-        $this->assertEquals(array('dnsbl.sorbs.net'), $this->rbl->getRBL());
+        $this->rbl->setBlacklists(array('dnsbl.sorbs.net'));
+        $this->assertEquals(array('dnsbl.sorbs.net'), $this->rbl->getBlacklists());
         $this->assertEquals(false, $this->rbl->isListed("mail.nohn.net"));
         $this->assertEquals(true,  $this->rbl->isListed("p50927464.dip.t-dialin.net"));
     }
