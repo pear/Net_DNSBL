0f17e0ab20983f51c38df003a9bee43f92e3b191
Sebastian Nohn <nohn@php.net>
2006-12-25 10:40:59 +0000

    * Unify Headers * Relicense from PHP License 3.0 to 3.01 * Add usage example Nagios probe
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_DNSBL/trunk@225743 c90b9560-bf6c-de11-be94-00142212c4b1

--- Net/DNSBL.php
+++ Net/DNSBL.php
@@ -10,13 +10,13 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
- * Net_RBL looks up an supplied host if it's listed in 1-n supplied
+ * Net_DNSBL looks up an supplied host if it's listed in 1-n supplied
  * Blacklists
  *
  * @category   Net
@@ -24,7 +24,7 @@
  * @author     Sebastian Nohn <sebastian@nohn.net>
  * @author     Ammar Ibrahim <fixxme@fixme.com>
  * @copyright  2004-2007 Sebastian Nohn <sebastian@nohn.net>
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Net_DNSBL
  * @see        Net_DNS
--- tests/testNetDNSBL.php
+++ tests/testNetDNSBL.php
@@ -1,19 +1,33 @@
 <?php
-//
-// +------------------------------------------------------------------------+
-// | PEAR :: Net_DNSBL                                                      |
-// +------------------------------------------------------------------------+
-// | Copyright (c) 2004-2007 Sebastian Nohn <sebastian@nohn.net>            |
-// +------------------------------------------------------------------------+
-// | This source file is subject to version 3.00 of the PHP License,        |
-// | that is available at http://www.php.net/license/3_0.txt.               |
-// | If you did not receive a copy of the PHP license and are unable to     |
-// | obtain it through the world-wide-web, please send a note to            |
-// | license@php.net so we can mail you a copy immediately.                 |
-// +------------------------------------------------------------------------+
-//
-// $Id$
-//
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * PEAR::Net_DNSBL
+ *
+ * This class acts as interface to generic Realtime Blocking Lists
+ * (RBL)
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * Net_DNSBL looks up an supplied host if it's listed in 1-n supplied
+ * Blacklists
+ *
+ * @category   Net
+ * @package    DNSBL
+ * @author     Sebastian Nohn <sebastian@nohn.net>
+ * @copyright  2004-2007 Sebastian Nohn <sebastian@nohn.net>
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Net_DNSBL
+ * @see        Net_DNS
+ * @since      File available since Release 1.0.0
+ */
 
 require_once "Net/DNSBL.php";
 require_once "PHPUnit/Framework/TestCase.php";
